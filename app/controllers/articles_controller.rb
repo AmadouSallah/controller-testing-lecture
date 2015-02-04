0619c99e4294ffcb72a34aea1d03545bd228b0a6
aespaldi <aespaldi@gmail.com>
2015-02-04 11:37:33 -0800

    update controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,31 +3,10 @@ get '/articles' do
   erb :index
 end
 
+get '/articles/:id' do
+  @article = Article.find(params[:id])
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# get '/articles' do
-#   @articles = Article.all
-#   erb :index
-# end
-
-# get '/articles/:id' do
-#   @article = Article.find(params[:id])
-# end
-
-# post '/articles' do
-#   Article.create(params)
-# end
+post '/articles' do
+  Article.create(params)
+end
